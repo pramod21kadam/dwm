dcfea4c22e7d0f857109557a7823c08eec3b2396
pramod kadam <pramod21dkadam@gmail.com>
2021-05-10 18:33:58 +0530

    Not patched with ipc but added the config files for it

--- config.h
+++ config.h
@@ -1,30 +1,36 @@
-/* See LICENSE file for copyright and license details. */
-
 // #include <X11/XF86keysym.h>
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int vertpad            = 0;        /* vertical padding of bar */
-static const int sidepad            = 0;        /* horizontal padding of bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+/****************************************************************************************
+ * Appearance
+ * Basic settings
+ ****************************************************************************************/
+static const unsigned int borderpx  = 1;	// border pixel of windows
+static const unsigned int gappx     = 0;	// gaps between windows
+static const unsigned int snap      = 32;	// snap pixel
+static const int showbar            = 1;	// 0 means no bar
+static const int vertpad            = 0;	// vertical padding of bar
+static const int sidepad            = 0;	// horizontal padding of bar
+static const int topbar             = 1;	// 0 means bottom bar
+static const unsigned int gappih    = 10;	// horiz inner gap between windows
+static const unsigned int gappiv    = 10;	// vert inner gap between windows
+static const unsigned int gappoh    = 10;	// horiz outer gap between windows and screen edge
+static const unsigned int gappov    = 10;	// vert outer gap between windows and screen edge
+static const int smartgaps          = 0;	// 1 means no outer gap when there is only one window
 
-static const unsigned int gappih    = 10;    // horiz inner gap between windows
-static const unsigned int gappiv    = 10;    // vert inner gap between windows
-static const unsigned int gappoh    = 10;    // horiz outer gap between windows and screen edge
-static const unsigned int gappov    = 10;    // vert outer gap between windows and screen edge
-static const int smartgaps          = 0;     // 1 means no outer gap when there is only one window
 
-// static const char *fonts[]          = { "Hack:pixelsize=16:antialias=true:autohint=true" };
-// static const char dmenufont[]       = "Hack:pixelsize=16:antialias=true:autohint=true";
+/****************************************************************************************
+ * Fonts
+ ****************************************************************************************/
 // static const char *fonts[]          = { "monospace:size=14" };
 // static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]		= { "FontAwesome:pixelsize=16:antialias=true:autohint=true" };
+static const char dmenufont[]		= "FontAwesome:pixelsize=16:antialias=true:autohint=true";
 
-static const char *fonts[]          = { "FontAwesome:pixelsize=16:antialias=true:autohint=true" };
-static const char dmenufont[]       = "FontAwesome:pixelsize=16:antialias=true:autohint=true";
 
+/****************************************************************************************
+ * Colors
+ * Basic settings
+ ****************************************************************************************/
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -38,32 +44,35 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_gray,  col_cyan  },
 };
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "", "" };
 
+/****************************************************************************************
+ * Tags and rules
+ * class      instance    title       tags mask	isfloating   monitor
+ ****************************************************************************************/
+static const char *tags[] = { "1", "2", "3", "", "" };
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask	isfloating   monitor */
-//	{ "Gimp",     NULL,       NULL,       0,	1,           -1 },
 	{ .class = "Firefox",  .instance = NULL,      .title = NULL,       .tags = 1<<4,	.isfloating = 0,           -1 },
 };
 
-/* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+/****************************************************************************************
+ * Layouts
+ * symbol	arrange		function 
+ ****************************************************************************************/
+static const float mfact     = 0.5;	// factor of master area size [0.05..0.95]
+static const int nmaster     = 1;	// number of clients in master area
+static const int resizehints = 1;	// 1 means respect size hints in tiled resizals
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "T",      tile },    /* first entry is default */
-	{ "F",      NULL },    /* no layout function means floating behavior */
+	{ "T",      tile },		// first entry is default */
+	{ "F",      NULL },		// no layout function means floating behavior */
 	{ "M",      monocle },
 };
 
-/* key definitions */
+
+
+/*****************************************************************************************
+ * key definitions 
+ ****************************************************************************************/
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -71,24 +80,26 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+
+/****************************************************************************************
+ * Helper for spawning shell commands in the pre dwm-5.0 fashion
+ ****************************************************************************************/
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-/* commands */
-
 // static const char *fonts[]          = { "monospace:size=14" };
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "tilix", NULL };
-
 // Brightness
 static const char *brightness_up[]	= { "/home/pramodkadam/.local/bin/brightness.sh" ,	"up",	NULL };
 static const char *brightness_down[] 	= { "/home/pramodkadam/.local/bin/brightness.sh" ,	"down",	NULL };
-
 // Volume
 static const char *volume_up[]		= { "/home/pramodkadam/.local/bin/volume.sh",		"up",	NULL};
 static const char *volume_down[]	= { "/home/pramodkadam/.local/bin/volume.sh",		"down", NULL};
 
+
+/****************************************************************************************
+ * Commands
+ ****************************************************************************************/
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -131,8 +142,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,         	XK_f,        	togglefloating, {0} },
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/****************************************************************************************
+* Button definitions
+*  click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin   
+****************************************************************************************/
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
@@ -148,3 +161,25 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+
+
+ /****************************************************************************************
+* Sock for dwm-msg
+****************************************************************************************/
+static const char *ipcsockpath = "/tmp/dwm.sock";
+static IPCCommand ipccommands[] = {
+  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
+  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
+  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
+};
